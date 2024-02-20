f5025b5ef1729a613cba723f9938547ed418dc7c
hariz <mhnriz@outlook.com>
2024-02-20 18:10:45 +0800

    modified:   level1_main.c 	new file:   level1_main.exe 	deleted:    level1_main.o

--- level1_main.c
+++ level1_main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
-int main(){
+void help();
+void play_game();
 
+// MAIN
+int main()
+{
+    int choice;
     printf("Welcome to Text Twist!\n\n");
-    printf("1. New game\2. Help\n3. Exit\nPlease choose your option (1~3)");
+    printf("1. New game\n2. Help\n3. Exit\nPlease choose your option (1~3): ");
+    do
+    {
+        scanf("%d", choice);
+        if (choice == 1)
+            play_game();
+        else if (choice == 2)
+            help();
 
+    } while (0);
+
+    return 0;
+}
+
+// FUNCTIONS
+void play_game()
+{
+}
+
+void help()
+{
+
+    printf("Instruction on How To Play.\n\n");
+    /*
+        INSERT INSTRUCTION
+    */
+    printf("Press ENTER to go back");
+    getc(stdin);
 }
