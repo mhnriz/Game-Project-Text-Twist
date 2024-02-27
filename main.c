e67861ef4bd1a1cda05ce68406f6021081c9c3da
mhnriz <mhnriz@outlook.com>
2024-02-27 02:54:02 +0000

    new file:   main.c

---
+++ main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void help();
+void play_game();
+void menu();
+void game_set();
+void flush();
+
+// MAIN
+int main()
+{
+    menu();
+    return 0;
+}
+
+// FUNCTIONS
+void play_game()
+{
+    system("clear");
+    printf("Here is your puzzle:\n\n");
+    game_set();
+    while (1);
+}
+
+void help()
+{
+    printf("Instruction on How To Play.\n\n");
+    /*
+        INSERT INSTRUCTION
+    */
+    printf("Press ENTER to go back");
+    getchar();
+    flush();
+}
+
+void game_set()
+{
+    char line[10];
+    int x = 1; // no. for gameset
+    FILE *file;
+    file = fopen("GameSets.txt", "r");
+    while (0 < x)
+    {
+    	fgets(line, 10, file);
+    	x--;
+    }
+    printf("%s\n", line);
+}
+
+void menu()
+{
+	do{
+    	int choice = 0;
+    	//system("clear");
+    	printf("Welcome to TEXT TWIST!\n\n");
+    	printf("1. New game\n2. Help\n3. Exit\nPlease choose your option (1~3): ");
+        scanf("%d", &choice);
+        if(choice == 1)
+        	play_game();
+        else if(choice == 2)
+        	help();
+        else if(choice == 3)
+        	printf("Thanks for playing!!\nExiting...\n");
+    	else
+    		printf("Invalid input! Try again.\n");
+    }while (1);
+}
+
+void flush()
+{
+	int c;
+	while ((c = getchar()) != '\n' && c != EOF)
+	{
+		printf("Unwanted input detected! Press ENTER to go back");
+		while((c = getchar()) != '\n' && c != EOF);
+	}
