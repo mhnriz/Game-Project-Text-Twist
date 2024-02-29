6c727fc3bdaef45930de5943d24dca79c595d795
mhnriz <mhnriz@outlook.com>
2024-02-29 06:47:15 +0000

    modified:   main.c 	modified:   test.c 	modified:   wordlist_Game1.txt

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void help();
 void play_game();
 void menu();
 void game_set();
 void flush();
+void wordlist(int *point, char guess[10]);
 
 // MAIN
 int main()
@@ -17,10 +19,16 @@ int main()
 // FUNCTIONS
 void play_game()
 {
+	int points = 0;
+	char guess[16];
     system("clear");
     printf("Here is your puzzle:\n\n");
-    game_set();
-    while (1);
+    do{
+    	game_set();	
+    	printf("Guess a word or END to quit: ");
+    	scanf("%s",guess);
+    	wordlist(&points, guess);
+    }while (strcmp(guess,"END") != 0);		//strcmp: string comparison; if true returnn 0
 }
 
 void help()
@@ -37,12 +45,12 @@ void help()
 void game_set()
 {
     char line[10];
-    int x = 1; // no. for gameset
-    FILE *file;
-    file = fopen("GameSets.txt", "r");
+    int x = 1;		// no. for gameset
+    FILE *set;
+    set = fopen("GameSets.txt", "r");
     while (0 < x)
     {
-    	fgets(line, 10, file);
+    	fgets(line, 10, set);
     	x--;
     }
     printf("%s\n", line);
@@ -50,8 +58,8 @@ void game_set()
 
 void menu()
 {
+	int choice = 0;
 	do{
-    	int choice = 0;
     	//system("clear");
     	printf("Welcome to TEXT TWIST!\n\n");
     	printf("1. New game\n2. Help\n3. Exit\nPlease choose your option (1~3): ");
@@ -64,7 +72,7 @@ void menu()
         	printf("Thanks for playing!!\nExiting...\n");
     	else
     		printf("Invalid input! Try again.\n");
-    }while (1);
+    }while (choice != 3);
 }
 
 void flush()
@@ -75,3 +83,20 @@ void flush()
 		printf("Unwanted input detected! Press ENTER to go back");
 		while((c = getchar()) != '\n' && c != EOF);
 	}
+}
+
+void wordlist(int *points, char guess[0])
+{
+	char string[10];
+	FILE *list;
+	list = fopen("wordlist_Game1.txt", "r");
+	while(fgets(string,10,list)){
+		string[strcspn(string,"\n")]= '\0';		//strcspn() string till '\n' and replace with '\0'.
+		if(strcmp(string,guess)==0)
+			printf("%s: VALID WORD\n", guess);
+			*points += 2;
+			
+	}
+	fclose(list);
+	
+}
--- test.c
+++ test.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
-
-int main(){
-
-    char myString[100];
-    FILE *file;
-    file = fopen("GameSets.txt", "r");
-    fgets(myString, 100, file);
-    printf("%s", myString);
-    return 0;
+#include <string.h>
+int main()
+{
+	int i, points =0;
+	char guess[10], string[10];
+	FILE *list;
+	list = fopen("wordlist_Game1.txt", "r");
+	
+	while(fgets(string,10,list) != NULL){
+		printf("%s", string);
+		scanf("%s",guess);
+		string[strcspn(string,"\n")] = '\0';		//strcspn(); read line till "\n" and replace it with \0.
+		if(strcmp(guess,string) == 0)
+			printf("yayy");
+		}
+	/*do{
+		fgets(string,10,list);
+		if(strcmp(string,guess) ==0
+			points += 1;
+	}while*/
 }       
