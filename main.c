f36ce55ace4f05509c7c9ae0eaefa0e8813cc4dd
mhnriz <mhnriz@outlook.com>
2024-03-03 06:37:57 +0000

    modified:   main.c 	modified:   main.o

--- main.c
+++ main.c
@@ -1,157 +1,150 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-void help();
-void play_game();
 void menu();
+void play_game();
 void game_set();
+void wordlist(int *check, char guess[15]);
+int check_guess(int check, char guess[15], int *counter);
+void help();
 void flush();
-void guesses(int *check, char guess[10], int *end);
 void ended(int points);
+//MAIN
 
-// MAIN
-int main()
-{
+int main(){
 	menu();
 	return 0;
 }
 
-// FUNCTIONS
-void play_game()
-{
-	int check = 0, points = 0, i = 0, j, k, end;
-	char guess[16];
-	char guessed[25][10];
+//FUNCTIONS
 
+void menu(){
+	int choice = 0;
+	do{
+		system("clear");
+		printf("Welcome to TEXT TWIST!\n\n");
+		printf("1. New game\n2. Help\n3. Exit\n");
+		printf("Please choose your option (1~3): ");
+		scanf("%d", &choice);
+		if(choice == 1) play_game();
+		else if(choice == 2) help();
+		else if(choice == 3) printf("Thanks for playing!!\nExiting...\n");
+		else printf("Invalid input! Try again.\n");
+	}while(choice != 3);
+}
+
+void play_game(){
+	char guess[15];
+	int check, counter = 0, i = 0, j, guessed, points = 0;
+	char correct_guess[25][10];
+	
 	system("clear");
 	printf("Here is your puzzle:\n\n");
-
-	do
-	{
+	do{
 		game_set();
-		if (check)
-		{
-			for(j = i;j >= 0; j--)
-			{
-				if (strcmp(guessed[j],guess) == 0)
-				{
-					k = 1;
-					break;
-				}		
-			}
-			if (k == 1)
-				printf("You have already guessed it! \n");
-			else
-			{
-				points += 2 * strlen(guess);
-				strcpy(guessed[i], guess);
-				i++;
-			}
-			k = 0;
-		}
-		if (i >= 1)
-		{
-			printf("points so far: %d\n", points);
-			printf("You have made the following words so far: \n");
-			for (j = 0; j <= i; j++)
-				printf("%s ", guessed[j-1]);
-			printf("\n\n");
-		}
-		if(i >= end)
-			break;
 		printf("Guess a word or END to quit: ");
 		scanf("%s", guess);
-		if(strcmp(guess, "END") == 0)	// strcmp: string comparison; if true return 0
-			break;
-		guesses(&check, guess, &end);
-	} while (1);
-	
+		if(strcmp(guess,"END") == 0) break;
+		check = 0;
+		guessed=0;
+		wordlist(&check, guess);
+		for(j = i;j >= 0; j--){
+			if (strcmp(correct_guess[j],guess) == 0){	
+				printf("You have already guessed it!\n");
+				printf("-----------------------------------------------\n");
+				guessed = 1;
+				break;
+			}		
+		}
+		if(guessed == 0) check_guess(check, guess, &counter);
+		if(check == 1 && guessed == 0){
+			strcpy(correct_guess[counter-1],guess);
+			points += 2*strlen(guess);
+		}
+		if(counter > 0){
+			printf("You made the following words so far: ");
+			i=0;
+			while(i < counter){
+				printf("%s ",correct_guess[i]);
+				i++;
+			}
+			printf("\n");
+			
+			
+		}
+	}while(counter < 20);
 	ended(points);
 }
 
-void help()
-{
+void game_set(){
+	int x = 1;		// for choosing gameset 1~3
+	char line[10];
+	FILE *fp;
+	fp = fopen("GameSets.txt","r");
+	while(x > 0){
+		fgets(line,10,fp);
+		x--;
+	}
+	fclose(fp);
+	printf("%s\n", line);
+}
+
+void wordlist(int *check, char guess[15]){
+	char string[10];
+	FILE *fp;
+	fp = fopen("wordlist_Game1.txt","r");
+	while(fgets(string,10,fp)!=NULL){
+		string[strcspn(string, "\n")] = '\0';		// read string till '\n' and replace it with '\0'
+		if(strcmp(string,guess) == 0) *check = 1;
+	}
+	fclose(fp);
+}
+int check_guess(int check, char guess[15], int *counter){
+	
+	if(check == 1){
+		printf("%s: VALID WORD\n", guess);	
+		printf("-----------------------------------------------\n");
+		*counter = *counter + 1;
+		return 0;
+	}
+	else{
+		int i = 0;
+		do{
+			printf("%c\n",guess[i]);
+			if(strlen(guess)<3){
+				printf("TOO SHORT\n");
+				printf("-----------------------------------------------\n");
+				return 0;
+			}
+			else if(isdigit(guess[i]) || !isalnum(guess[i])){
+				printf("Invalid symbol/number %c\n",guess[i]);
+				printf("-----------------------------------------------\n");
+				return 0;
+			}
+			i++;
+		}while(i < strlen(guess));
+		printf("%s: INVALID WORD\n", guess);	
+		printf("-----------------------------------------------\n");
+		return 0;
+	}
+}
+
+void help(){
 	printf("\nInstruction on How To Play.\n\n");
 	printf("You will be given a word.\nYou may guess using only the letter the word has.\nThe longer the word you guessed.\nThe more points you will achieve.\nGOODLUCK :)\n\n");	
 	printf("Press ENTER to go back");
 	flush();
 }
 
-void game_set()
-{
-	char line[10];
-	int x = 1; // no. for gameset
-	FILE *set;
-	set = fopen("GameSets.txt", "r");
-	while (0 < x)
-	{
-		fgets(line, 10, set);
-		x--;
-	}
-	printf("%s\n", line);
-}
-
-void menu()
-{
-	int choice = 0;
-	do
-	{
-		system("clear");
-		printf("Welcome to TEXT TWIST!\n\n");
-		printf("1. New game\n2. Help\n3. Exit\nPlease choose your option (1~3): ");
-		scanf("%d", &choice);
-		if (choice == 1)
-			play_game();
-		else if (choice == 2)
-			help();
-		else if (choice == 3)
-			printf("Thanks for playing!!\nExiting...\n");
-		else
-			printf("Invalid input! Try again.\n");
-	} while (choice != 3);
-}
-
-void flush()
-{
+void flush(){
 	int c;
-	getchar();		//not sure why i need to do this separately to make it work
-	while ((c = getchar()) != '\n' && c != EOF)		//loop getchar till no input buffer
-	{
+	getchar();
+	while((c = getchar()) != '\n'){
 		printf("Unwanted input detected! Press ENTER to go back");
-		while ((c = getchar()) != '\n' && c != EOF);
+		while((c = getchar()) != '\n');
 	}
-}
-
-void guesses(int *check, char guess[10], int *end)
-{
-	int i = 0, j;
-	*end = 0;
-	char string[10];
-	FILE *list;
-	list = fopen("wordlist_Game1.txt", "r");
-	while (fgets(string, 10, list))
-	{
-		string[strcspn(string, "\n")] = '\0';		// strcspn() read string till '\n' and replace with '\0'.
-		if (strcmp(string, guess) == 0)
-			j = 1;
-		i++;
-	}
-	*end = i;
-	if (j == 1)
-	{
-		printf("%s: VALID WORD\n", guess);
-		printf("----------------------------------------------\n");
-
-		*check = 1;
-	}
-	else
-	{
-		printf("%s: INVALID WORD\n", guess);
-		printf("----------------------------------------------\n");
-		*check = 0;
-	}	
-	fclose(list);
 }
 
 void ended(int points)
@@ -166,7 +159,7 @@ void ended(int points)
 	{
 		printf("%s",string);
 	}
-	
+	fclose(fp);
 	printf("\nThank you for playing!!\n\n");
 	printf("Press ENTER to go back");
 	flush();
