d97d1c0efd2523de8b1d101b074359d6b1a5dbd8
Hariz <49497959+mhnriz@users.noreply.github.com>
2025-11-24 10:42:56 +0800

    Delete backup directory

--- backup/gfx_element.h
+++
@@ -1,9 +0,0 @@
-void button(int x, int y, int width, int height, char text[]);
-void button_clicked(int x, int y, int width, int height, char text[]);
-void background_brick(int x, int y);
-void foreground_brick(int x, int y);
-void background(int x, int y);
-void foreground(int x, int y);
-void torch(int x, int y);
-void display_text(int x, int y, char input[10]);
-void input_box(int x, int y, int width, int height, char input[10], int *redo);
